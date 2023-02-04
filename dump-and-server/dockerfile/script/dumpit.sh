abcd13be3fdab15d1357796731721c7fbb59dd5e
terria1020 <jaehan1346@gmail.com>
2023-02-04 23:33:25 +0900

    feat: dump 후 사용 가능한 서버를 올리는 서비스 구현
    feat: dump 후 사용 가능한 서버를 올리는 서비스 구현
    - Dockerfile에서 사용할 쉘 스크립트 작성
    - 환경변수 저장파일 생성
    - compose 파일 작성

---
+++ dump-and-server/dockerfile/script/dumpit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "service mysql on for use client"
+service mysql start
+
+echo "wait for db init" 
+sleep 20
+
+echo "dump to it"
+mysqldump -u $ORG_USER -p$ORG_PASSWORD --host $ORG_HOST --databases $ORG_DB --no-data --set-gtid-purged=OFF --column-statistics=0 > /root/all_ddl.sql
+sed "s/$ORG_HOST/localhost/g" /root/all_ddl.sql > /root/all_ddl.sql.new
+mv /root/all_ddl.sql.new /root/all_ddl.sql
+
+echo "drop and create database"
+echo y | mysqladmin -u root -p$MYSQL_ROOT_PASSWORD --host $MYSQL_SERVICENAME --port 3306 drop $ORG_DB
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD --host $MYSQL_SERVICENAME --port 3306 create $ORG_DB
+
+echo "pump to it"
+mysql -u root -p$MYSQL_ROOT_PASSWORD --host $MYSQL_SERVICENAME --port 3306 $ORG_DB < /root/all_ddl.sql
+
+echo "dump finished!"
