befc3a4455c7c12dbdd110fc707f309c984de8f2
terria1020 <jaehan1346@gmail.com>
2023-02-05 23:31:09 +0900

    feat: with-redis script 추가

---
+++ script/only-server-with-redis-start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "only-server-with-redis start"
+cd ../only-server-with-redis/
+docker compose up -d
---
+++ script/only-server-with-redis-stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "only-server-with-redis stop"
+cd ../only-server-with-redis/
+docker compose down
